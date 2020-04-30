bacc4482bcab8596f43af8743aa4da4dc73b0765
Marta Kocmidova <mochomurka6@seznam.cz>
2020-04-30 19:37:52 +0200

    api for smart brain app

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+const clarifai = require('clarifai')
+
+// I must add my own API key here from Clarify - check
+const app = new Clarifai.App({
+  apiKey: '1c54491670014f97952c5252837adaa9'
+});
+
+
+const handleApiCall = (req, res) => {
+  app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+      .then(data => {
+        res.json(data)
+      })
+      .catch(err => res.status(400).json('unable to work with API'))
+}
+
+
+const handleImage = (db) => (req, res,) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+      .increment('entries', 1)
+      .returning('entries')
+      .then(entries => {
+        res.json(entries[0])
+      })
+      .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+  handleImage,
+  handleApiCall
+}
