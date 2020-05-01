bd416b5ef4911e93c5d448fad93444388d848cf0
Marta Kocmidova <mochomurka6@seznam.cz>
2020-05-01 12:24:54 +0200

    added gitignore

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors')
+const knex = require('knex')
+
+const register = require('./controllers/register')
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
+
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1', // = localhost
+    user : 'postgres',
+    password : 'test',
+    database : 'smart_brain'
+  }
+});
+
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => { res.send(db.user) })
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+
+app.put('/image', image.handleImage(db)) // jiný styl zápisu - tady nepotřebujeme req, res, ale přididáme je do image.js
+
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`app is running on port 8080 ${PORT}`)
+})
